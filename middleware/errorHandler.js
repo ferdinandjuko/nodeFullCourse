4f092c76bd9d14df60a9b4a183593f99632df83c
rferdinandjuko <rferdinandjuko@gmail.com>
2026-01-27 12:51:49 +0300

    middleware/errorHandler err.name err.message

---
+++ middleware/errorHandler.js
@@ -0,0 +1,9 @@
+import { logEvents } from './logEvents.js';
+
+const errorHandler = (err, req, res, next) => {
+    logEvents(`${err.name}: ${err.message}`, 'errLog.log');
+    console.error(err.stack);
+    res.status(500).send(err.message);
+}
+
+export default errorHandler;
