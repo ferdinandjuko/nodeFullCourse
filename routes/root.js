bef5b2a357b544764175279e9ce7cf389db0e0d3
rferdinandjuko <rferdinandjuko@gmail.com>
2026-01-28 15:16:45 +0300

    home page handled from root.js

--- routes/root.js
+++ routes/root.js
@@ -5,3 +5,10 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = express.Router();
+
+router.get(['/', '/index.html', '/index'], (req, res) => {
+    // res.sendFile('./views/index.html', { root: __dirname });
+    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+})
+
+export default router;
