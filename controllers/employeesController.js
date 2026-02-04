7b03335c9ab274c994e20e46fe743c09649b20a3
rferdinandjuko <rferdinandjuko@gmail.com>
2026-02-04 18:39:42 +0300

    update employee by req.body.id

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -29,10 +29,17 @@ const createNewEmployee = (req, res) => {
 }
 
 const updateEmployee = (req, res) => {
-    res.json({
-        "firstname": req.body.firstname,
-        "lastname": req.body.lastname
-    });
+    const employee = data.employees.find(find(emp => emp.id === parseInt(req.body.id)));
+    if (!employee) {
+        return res.status(400).json({ 'message': `Employee ID ${req.body.id} not found` });
+    }
+    if (req.body.firstname) employee.firstname = req.body.firstname;
+    if (req.body.lastname) employee.lastname = req.body.lastname;
+
+    const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
+    const unsortedArray = [...filteredArray, employee];
+    data.setEmployees(unsortedArray.sort((a, b) => a.id ? 1 : a.id < b.id ? -1 : 0));
+    res.json(data.employees);
 }
 
 const deleteEmployee = (req, res) => {
