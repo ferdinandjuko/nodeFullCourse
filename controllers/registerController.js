9fa3e17e536c2727aab8ee32fa1cdef75123f080
rferdinandjuko <rferdinandjuko@gmail.com>
2026-02-07 12:12:51 +0300

    handleNewUser duplicate by find

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,6 +1,7 @@
 import fsPromises from 'fs/promises';
 import { fileUrlToPath } from 'url'
 import path from 'path';
+import bcrypt from 'bcrypt';
 
 const __filename = fileUrlToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -8,4 +9,12 @@ const __dirname = path.dirname(__filename);
 const usersDB = {
     users: JSON.parse(await fsPromises.readFile(path.join(__dirname, '..', 'model', 'users.json'))),
     setUser: function (data) { this.users = data }
+}
+
+const handleNewUser = async (req, res) => {
+    const { user, pwd } = req.body;
+    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are erquired' });
+    // check for duplicate username in the db
+    const duplicate = usersDB.find(user => user.username === user);
+    if (duplicate) return res.sendStatus(409); // Conflict
 }
