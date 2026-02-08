cfcf1ea090c6c9f8d516d12803eb16c3c79f8c21
rferdinandjuko <rferdinandjuko@gmail.com>
2026-02-08 08:43:05 +0300

    register API to handle new user adding

---
+++ routes/api/register.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import * as registerController from '../../controllers/registerController.js'
+
+const router = express.Router();
+
+router.post('/', registerController.handleNewUser);
+
+export default router;
