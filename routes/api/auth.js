7c635eca5a98c9794a44abba876f3df6b6f27147
rferdinandjuko <rferdinandjuko@gmail.com>
2026-02-12 03:42:46 +0300

    use post method to authController.handleLogin

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,3 +2,7 @@ import express from 'express';
 import * as authController from '../../controllers/authController.js'
 
 const router = express.Router();
+
+router.post('/', authController.handleLogin)
+
+export default router;
