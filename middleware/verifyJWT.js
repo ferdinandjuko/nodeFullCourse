c83d4a9e289675d85bd853ae577372bcfc2151bf
rferdinandjuko <rferdinandjuko@gmail.com>
2026-02-15 16:11:08 +0300

    fns jwt.verify() to verify token match with ACCES_TOKEN_SECRET

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -6,4 +6,16 @@ const verifyJWT = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     if (!authHeader) return res.sendStatus(401); // Unauthorized
     console.log(!authHeader); // Bearer token
+    const token = authHeader.split(' ')[1];
+    jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        (err, decoded) => {
+            if (err) return res.sendStatus(403); // invalid token
+            req.user = decoded.username;
+            next();
+        }
+    );
 }
+
+export default verifyJWT;
