7deb51bfd64a1ca37c558aab9da74c38630a949f
rferdinandjuko <rferdinandjuko@gmail.com>
2026-02-16 09:58:01 +0300

    manage registerController.handleNewUser fns

--- routes/register.js
+++ routes/register.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import * as registerController from '../../controllers/registerController.js'
+import * as registerController from '../controllers/registerController.js'
 
 const router = express.Router();
 
