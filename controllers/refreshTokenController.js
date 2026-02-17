d68ad3abe957f41d45079a4f69ac1396b101818c
rferdinandjuko <rferdinandjuko@gmail.com>
2026-02-17 17:36:34 +0300

    refreshTokenController to handle refreshToken

---
+++ controllers/refreshTokenController.js
@@ -0,0 +1,20 @@
+import fsPromises from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const usersDB = {
+    users: JSON.parse(await fsPromises.readFile(path.join(__dirname, '..', 'model', 'users.json'))),
+    setUsers: function (data) { this.users = data }
+}
+
+const handleRefreshToken = (req, res) => {
+    const cookie = req.cookies;
+}
+
+export { handleRefreshToken };
