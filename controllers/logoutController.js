8de257b1e74f3c59b80dc970220929b5158055eb
rferdinandjuko <rferdinandjuko@gmail.com>
2026-02-18 14:32:11 +0300

    check if req.cookies.jwt is not ereased

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -11,6 +11,11 @@ const usersDB = {
 }
 
 const handleLogout = async (req, res) => {
+    // On client, also delete the acessToken
+
+    const cookies = req.cookies;
+    if (!cookies?.jwt) return res.sendStatus(204); // No content
+    const refreshToken = cookies.jwt;
 }
 
 export { handleLogout };
