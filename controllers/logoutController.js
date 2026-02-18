bed7111272f9fb99654aa64ec590f6e532fc75df
rferdinandjuko <rferdinandjuko@gmail.com>
2026-02-18 14:12:58 +0300

    logoutController is an async function

---
+++ controllers/logoutController.js
@@ -0,0 +1,16 @@
+import fsPromises from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const usersDB = {
+    users: JSON.parse(await fsPromises.readFile(path.join(__dirname, '..', 'model', 'users.json'))),
+    setUsers: function (data) { this.users = data }
+}
+
+const handleLogout = async (req, res) => {
+}
+
+export { handleLogout };
