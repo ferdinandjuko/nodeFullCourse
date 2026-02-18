7225cc2c75ed5f386f367a257c1ecc0c0d6a2a77
rferdinandjuko <rferdinandjuko@gmail.com>
2026-02-18 13:52:38 +0300

    refreshTokenRouter from refreshTokenCntrl and ./routes/refresh.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ import employeesRouter from './routes/api/employees.js';
 import rootRouter from './routes/root.js';
 import registerRouter from './routes/register.js';
 import authRouter from './routes/auth.js';
+import refreshRouter from './routes/refresh.js';
 
 const app = express();
 const PORT = process.env.PORT || 3500;
@@ -42,6 +43,7 @@ app.use('/', express.static(path.join(__dirname, '/public')));
 app.use('/', rootRouter);
 app.use('/register', registerRouter);
 app.use('/auth', authRouter);
+app.use('/refresh', refreshRouter);
 
 app.use(verifyJWT); // Like waterfall, everything after this line wil use the verified jwt
 app.use('/employees', employeesRouter);
