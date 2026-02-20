a3c79748b9f46cb1f2ac486d7ad1781cc8ed6198
rferdinandjuko <rferdinandjuko@gmail.com>
2026-02-20 13:12:36 +0300

    cors options json using callBack if there wasn't allowed by CORS

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,13 +1,8 @@
-const whitelist = [
-    'https://www.yoursite.com',
-    'http:127.0.0.1:5500',
-    'http://127.0.0.1:3500',
-    'http://localhost:3500'
-];
+import allowedOrigins from './allowedOrigins.js';
 
 const corsOptions = {
     origin: (origin, callBack) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callBack(null, true)
         } else {
             callBack(new Error('Not allowed by CORS'));
