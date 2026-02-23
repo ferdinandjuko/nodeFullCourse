cc4ddc09993a9d78630cc218b2e6019193d70f78
rferdinandjuko <rferdinandjuko@gmail.com>
2026-02-23 09:11:29 +0300

    affecting spread alloedRoles value in array

--- middleware/verifyRoles.js
+++ middleware/verifyRoles.js
@@ -1,6 +1,9 @@
 const verifyRoles = (...allowedRoles) => {
     return (req, res, next) => {
         if (!req.roles) return res.sendStatus(401);
+        const rolesArray = [...allowedRoles];
+        console.log(rolesArray);
+        console.log(req.roles);
     }
 }
 
