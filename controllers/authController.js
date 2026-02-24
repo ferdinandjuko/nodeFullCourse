f056ece3e8ad8469c6f87b80bc700b2e50b7d105
rferdinandjuko <rferdinandjuko@gmail.com>
2026-02-24 17:15:03 +0300

    usersDB.setUsers([...otherUsers, currentUser]);

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,8 +3,6 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
