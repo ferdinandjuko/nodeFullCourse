be12a6a2998a7a21a16df64331167b0a14b6784b
rferdinandjuko <rferdinandjuko@gmail.com>
2026-02-25 15:05:36 +0300

    config fns connectDB async to acces .env.DATABASE_URI

--- config/dbConn.js
+++ config/dbConn.js
@@ -3,4 +3,12 @@ dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 import mongoose from 'mongoose';
 
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URI);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
 export default connectDB;
