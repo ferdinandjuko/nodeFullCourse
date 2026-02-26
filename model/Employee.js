46d0a39dc847cbc355ee50be1f9ed59823212a46
rferdinandjuko <rferdinandjuko@gmail.com>
2026-02-26 17:14:37 +0300

    new Schema employee attribut required

--- model/Employee.js
+++ model/Employee.js
@@ -3,6 +3,14 @@ import mongoose from 'mongoose';
 const Schema = mangoose.Schema;
 
 const employeeSchema = new Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    }
 });
 
 export default mongoose.model('Employee', employeeSchema);
