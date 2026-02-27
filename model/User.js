ec031b564bc4a8451adadc1c2c63304c1cbe660a
rferdinandjuko <rferdinandjuko@gmail.com>
2026-02-27 17:17:31 +0300

    usual attribut for User Schema

--- model/User.js
+++ model/User.js
@@ -2,6 +2,15 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    refreshToken: String
 });
 
 export default mongoose.model('User', userSchema);
