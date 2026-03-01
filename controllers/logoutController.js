c4a3d72d00a14aa7fc155a2e1fd5b9db73296869
rferdinandjuko <rferdinandjuko@gmail.com>
2026-03-01 14:57:00 +0300

    clearCookie from database by refreshToken attribut

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,38 +1,27 @@
-import fsPromises from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import User from '../model/User.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const usersDB = {
-    users: JSON.parse(await fsPromises.readFile(path.join(__dirname, '..', 'model', 'users.json'))),
-    setUsers: function (data) { this.users = data }
-}
 
 const handleLogout = async (req, res) => {
     // On client, also delete the acessToken
 
     const cookies = req.cookies;
-    if (!cookies?.jwt) return res.sendStatus(204); // No content
+    if (!cookies?.jwt) return res.sendStatus(205); // No content
     const refreshToken = cookies.jwt;
 
     // Is refresh token in DB ?
-    const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
+    const foundUser = await User.findOne({ refreshToken }).exec();
     if (!foundUser) {
         res.clearCookie('jwt', { httpOnly: true });
         return res.sendStatus(204); // Success but No Content
     }
-
-
+    const isDev = process.env.NODE_ENV !== 'production'
     // Delete refreshToken in db
-    const otherUsers = usersDB.users.filter(person => person.refreshToken !== foundUser.refreshToken);
-    const currentUser = { ...foundUser, refreshToken: '' };
-    usersDB.setUsers([...otherUsers, currentUser]);
-    await fsPromises.writeFile(
-        path.join(__dirname, '..', 'model', 'users.json'),
-        JSON.stringify(usersDB.users)
-    );
+    foundUser.refreshToken = '';
+    await foundUser.save();
 
     res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true }); // Secure: true - Only serves on http
     res.sendStatus(204);
