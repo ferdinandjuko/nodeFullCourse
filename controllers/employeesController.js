b334b6bbf12b6c5c659ed1e7bd167ccad996bc04
rferdinandjuko <rferdinandjuko@gmail.com>
2026-03-06 16:14:36 +0300

    await Employee.create({firstname and lastname})

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -6,23 +6,22 @@ const getAllEmployees = async (req, res) => {
     res.json(employees);
 }
 
-const createNewEmployee = (req, res) => {
-    const newEmployee = {
-        id: data.employees[data.employees.length - 1].id + 1 || 1,
-        firstname: req.body.firstname,
-        lastname: req.body.lastname
+const createNewEmployee = async (req, res) => {
+    if (req?.body?.firstname || req?.body?.lastname) {
+        return res.status(400).json({ 'message': 'First and Last names are required' });
     }
 
-    if (!newEmployee.firstname || !newEmployee.lastname) {
-        res.status(400).json({ 'message': 'First and last names are required' });
+    try {
+        const newEmployee = await Employee.create({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname
+        });
+
+        res.status(201).json(newEmployee);
+    } catch (err) {
+        console.error(err);
     }
 
-    data.setEmployees([...data.employees, newEmployee]);
-    fsPromises.writeFile(
-        path.join(__dirname, '..', 'model', 'employees.json'),
-        JSON.stringify(data.employees)
-    );
-    res.status(201).json(data.employees);
 }
 
 const updateEmployee = (req, res) => {
